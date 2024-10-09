911b052164226d1cc2027174d7d5a2cc86dd7efb
rckalil <b46837@fgv.edu.br>
2024-10-09 15:43:35 -0300

    Arranging encapsulation of vm

--- src/vending_machine.py
+++ src/vending_machine.py
@@ -5,25 +5,25 @@ class VendingMachine():
     VendingMachine class
     
     Attributes:
-    - ID: str
-    - name: str
-    - owner_ID: str
-    - location: str
-    - products: list
+    - __ID: str
+    - __name: str
+    - __owner_ID: str
+    - __location: str
+    - __products: list
 
     Methods:
     - get_id: get the ID of the vending machine
     - get_name: get the name of the vending machine
-    - set_name: change the name of the vending machine
+    - __set_name: change the name of the vending machine
     - get_location: get the location of the vending machine
-    - set_location: change the location of the vending machine
+    - __set_location: change the location of the vending machine
     - get_owner_id: get the owner of the vending machine
     - get_products: get all products in the vending machine
     - get_product: get a specific product in the vending machine
     - add_product: add a product to the vending machine
     - remove_product: remove a product from the vending machine
     - update_product: update a product in the vending machine
-    - check_string: check if a string is valid
+    - __check_string: check if a string is valid
     """
     def __init__(self, ID, name, owner_ID, location, **kwargs):
         """
@@ -39,102 +39,39 @@ class VendingMachine():
         Returns:
         - None
         """
-        self.ID = ID
-        self.set_name(name)
-        self.set_owner_id(owner_ID)
-        self.set_location(location)
-        self.products = []
+        self.__ID = ID
+        self.__set_name(name)
+        self.__set_owner_id(owner_ID)
+        self.__set_location(location)
+        self.__products = []
     
     def get_id(self):
-        """
-        Get the ID of the vending machine
-
-        Parameters:
-        - None
-
-        Returns:
-        - str: ID of the vending machine
-        """
-        return self.ID
+        return self.__ID
     
     def get_name(self):
-        """
-        Get the name of the vending machine
-
-        Parameters:
-        - None
-
-        Returns:
-        - str: Name of the vending machine
-        """
-        return self.name 
+        return self.__name 
     
-    def set_name(self, name):
-        """
-        Change the name of the vending machine
-
-        Parameters:
-        - name (str): New name of the vending machine
-
-        Returns:
-        - None
-        """
-        self.name = name
+    def __set_name(self, name):
+        self.__name = name
 
     def get_location(self):
-        """
-        Get the location of the vending machine
-
-        Parameters:
-        - None
-
-        Returns:
-        - str: Location of the vending machine
-        """
-        return self.location
+        return self.__location
     
-    def set_location(self, location):
-        """
-        Change the location of the vending machine
-
-        Parameters:
-        - location (str): New location of the vending machine
-
-        Returns:
-        - None
-        """
-        self.location = location
+    def __set_location(self, location):
+        self.__location = location
 
     def get_owner_id(self):
-        """
-        Get the owner of the vending machine
-
-        Parameters:
-        - None
-
-        Returns:
-        - str: ID of the owner of the vending machine
-        """
-        return self.owner_ID
+        return self.__owner_ID
 
     def get_products(self):
-        """
-        Get all products in the vending machine
-
-        Parameters:
-        - None
-
-        Returns:
-        - list: List of products in the vending machine
-        """
-        return self.products
+        return self.__products
      
-    def get_product(self, *args):
+    def get_product(self, *kwargs):
         """
         Get a specific product in the vending machine
 
         Parameters:
-        - args (list): Contains parameters that identify the product to be retrieved
+        - kwargs (dict): Contains parameters that identify the product to be retrieved
 
         Returns:
         - dict: Product in the vending machine
@@ -153,25 +90,24 @@ class VendingMachine():
         """
         pass
 
-    def remove_product(self, *args):
+    def remove_product(self, *kwargs):
         """
         Remove a product from the vending machine
 
         Parameters:
-        - args (list): Contains parameters that identify the product to be removed
+        - kwargs (dict): Contains parameters that identify the product to be removed
 
         Returns:
         - None
         """
         pass
 
-    def update_product(self, *args, **kwargs):
+    def update_product(self, **kwargs):
         """
         Update a product in the vending machine
 
         Parameters:
-        - args (list): Contains parameters that identify the product to be updated
-        - kwargs (dict): Contains parameters for the product to be updated
+        - kwargs (dict): Contains parameters that identify the product to be updated and provide the new values
 
         Returns:
         - None
